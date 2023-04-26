1a6d00cb4931cae67e61c0f195cfe9e314568464
Daniel Durán <danieldurange@outlook.com>
2023-04-26 11:12:21 -0500

    01-commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require("colors");
+const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
+const Tarea = require("./models/tarea");
+const Tareas = require("./models/tareas");
+
+const main = async () => {
+  let opt = "";
+
+  const tareas = new Tareas();
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case "1":
+        const desc = await leerInput("Descripción: ");
+        tareas.crearTarea(desc);
+        break;
+
+      case "2":
+        console.log(tareas.listadoArr);
+        break;
+      default:
+        break;
+    }
+
+    await pausa();
+  } while (opt !== "0");
+};
+
+main();
